b83cb9255587fcd4176bb6baac8744ed45ef19b0
Stefanie Taepke <stefanie.taepke@liip.ch>
2016-12-07 18:04:44 +0100

    use ckan.site_url_internal

--- ckanext/datapusher/plugin.py
+++ ckanext/datapusher/plugin.py
@@ -89,7 +89,7 @@ class DatapusherPlugin(p.SingletonPlugin):
         datapusher_formats = config.get('ckan.datapusher.formats', '').lower()
         self.datapusher_formats = datapusher_formats.split() or DEFAULT_FORMATS
 
-        for config_option in ('ckan.site_url', 'ckan.datapusher.url',):
+        for config_option in ('ckan.site_url_internal', 'ckan.datapusher.url',):
             if not config.get(config_option):
                 raise Exception(
                     'Config option `{0}` must be set to use the DataPusher.'
